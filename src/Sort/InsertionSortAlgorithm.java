13d628f932af41372174d30eb389c7a1cd7ddf8b
hatef <hatef.alpoor@outlook.com>
2020-02-16 01:19:55 +0330

    insertion sort added

--- src/Sort/InsertionSortAlgorithm.java
+++ src/Sort/InsertionSortAlgorithm.java
@@ -5,6 +5,18 @@ import java.util.List;
 public class InsertionSortAlgorithm implements SortAlgorithm {
     @Override
     public <E extends Comparable<E>> List<E> sort(List<E> elements) {
-        return null;
+        for (int unsortedIndex = 1; unsortedIndex < elements.size(); unsortedIndex++){
+            E unsortedElement = elements.get(unsortedIndex);
+            int sortedIndex = unsortedIndex;
+            for (; sortedIndex > 0
+                    && elements.get(sortedIndex - 1).compareTo(unsortedElement) > 0; sortedIndex--){
+                elements.set(sortedIndex, elements.get(sortedIndex - 1));
+                }
+                elements.set(sortedIndex, unsortedElement);
+            }
+
+            return elements;
+        }
+
     }
-}
+
